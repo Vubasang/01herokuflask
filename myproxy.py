5ef2466ac98b3e0b091ebe88ef8f0be1fd7450ff
Ubuntu <azure1@mondoev.2frrsy0fm4ze1cbajvqrbolmkc.bx.internal.cloudapp.net>
2021-04-29 09:41:34 +0000

    prog

--- myproxy.py
+++ myproxy.py
@@ -1,47 +1,44 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask import request
+from werkzeug.utils import secure_filename
 import requests
 import json
+import os
 from datetime import datetime
 import mysql.connector
+from flask import send_from_directory
+
+UPLOAD_FOLDER = './images'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/", methods=['GET'])
-def hello_world():
-    msg = "Hello IVT-17!"
-    if request.args.get('url'):
-        ip_client = request.remote_addr
-        try:
-            r = requests.get(request.args['url'])
-            if r.status_code==200:
-                return r.content
-            else:
-                msg = str(r.status_code)
-        except:
-            msg = f"Не удалось загрузить {request.args.get('url')}"
-        try:
-            config = json.load(open('/config/config.json'))
-        except:
-            msg = f"Не удалось прочесть настройки из файла /config/config.json"
-        try:
-            mysql_server = config['ip']
-            cnx = mysql.connector.connect(user='myproxy@the-mysql', database='myproxy',
-                                          host=mysql_server, password='1234zxcv')
-            cursor = cnx.cursor()
-            now = datetime.now()
-            cursor.execute(
-                   "INSERT INTO myproxy.log VALUES(%s, %s, %s)",
-                   (now, ip_client, request.args['url'])
-            )
-            cnx.commit()
-        except:
-            msg = f"Не удалось записать информацию на сервер базы данных {mysql_server}"
-        
-    return '<h1>' + msg + '''</h1>
-    <form method="get">
-    <input type="text" name="url">
-    <input type="submit">
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route("/face", methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
     </form>
     '''
 
